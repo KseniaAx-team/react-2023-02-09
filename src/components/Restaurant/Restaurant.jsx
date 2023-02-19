a22c763af6fb9828e127a6e2bc3698a7549cb6b1
Vasyukhnova Kseniya <kseniya.vasyukhnova@ax-team.corp>
2023-02-19 15:13:46 +0300

    Home work 1

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Menu } from "../Menu/Menu";
+import { Reviews } from "../Reviews/Reviews";
 
-export const Restaurant = () => {
-  return <div>Restaurant</div>;
+export const Restaurant = ({restaurant}) => {
+  return (
+    <div>
+      <h2>{restaurant.name}</h2>
+      <Menu menu={restaurant.menu}/>
+      <Reviews reviews={restaurant.reviews}/>
+    </div>
+  )
 };
